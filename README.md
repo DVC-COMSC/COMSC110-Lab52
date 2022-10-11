23c9d991f53175a73f009ac05b243299c1ba2c01
Kyuwoong Lee <kwooler@gmail.com>
2022-10-11 07:10:38 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+## Lab 5-1
+
+### Print the even numbers from 10 to 20
+
+
+
+
+<!--
+### [Question 3](https://docs.google.com/document/d/1iMc8wbFr1h2pPzT8vaulgZehNN2hZANDZFNxekmzUq0/edit#bookmark=id.v7ydzultqkvr)
+-->
+
+
---
+++ main.cpp
@@ -0,0 +1,20 @@
+// 	Print value from begin to end
+//	begin = 10; end = 20;
+// 	10 11 12 13 14 15 16 17 18 19 20
+// 	Edit this program
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int 	i;
+	
+
+	for(i=0; i<10; i++)
+	{
+		cout << i << "\t";
+	}
+	cout << endl;
+	
+}
